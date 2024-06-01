16ae723022f65ca228c575a6fff60d5bd82aaa47
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-06-01 13:59:05 +0200

    /

--- user_db.sql
+++ user_db.sql
@@ -3,7 +3,7 @@ CREATE database users;
 \c users;
 CREATE TABLE accounts (
 IH char(64) NOT NULL,
-Verifier char(585) NOT NULL,
+Verifier char(844) NOT NULL,
 Username varchar(64) NOT NULL UNIQUE,
 ID  SERIAL PRIMARY KEY,
-address char(99) NOT NULL);
+address char(98) NOT NULL);
