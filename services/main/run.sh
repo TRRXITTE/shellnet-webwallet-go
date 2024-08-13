803a99797e14ca57c275c18aa8b105b091721ca6
traaitt | TRRXITTE Int., incorporate <traaitt@icloud.com>
2024-08-13 21:29:56 +0200

    update appicon

--- services/main/run.sh
+++ services/main/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-HOST_URI='http://localhost' \
-HOST_PORT=':8080' \
+HOST_URI='https://wallet.traaitt.com' \
+HOST_PORT=':443' \
 USER_URI='http://localhost:8081' \
 WALLET_URI='http://localhost:8082' \
-go run main.go init.go handlers.go utils.go
+/usr/local/go/bin/go run main.go init.go handlers.go utils.go
